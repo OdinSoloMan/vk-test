70a519e9e86913131948ab75ba7fb62e5b55b2f0
OdinSoloMan <yuraburov18@gmail.com>
2023-05-09 22:48:09 +0300

    Добавил базовую работу с пользователем и авторизацию

---
+++ VK-test/Infrastructure/Configurations/UsersConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.HasAlternateKey(b => b.Login);
+        }
+    }
+}
